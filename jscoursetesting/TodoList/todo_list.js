383386d94ec9332f65f3d286ffd2694bffd71283
sam-itlglobaltech <samuel@itlglobal.tech>
2024-04-07 11:24:32 -0400

    another commit including comments explaining the program

--- jscoursetesting/TodoList/todo_list.js
+++ jscoursetesting/TodoList/todo_list.js
@@ -1,10 +1,20 @@
+// Declaring a variable named taskInput that retrieves the HTML element
+// with the ID taskInput. The value entered by the users is assigned to
+// the variable taskInput
 const taskInput = document.getElementById("taskInput");
 
+// This fetches the button element responsible for adding tasks
 const addTaskBtn = document.getElementById("addTaskBtn");
+
+// This retrieves the unordered list elements where tasks are displayed
 const taskList = document.getElementById("taskList");
+
+// This access the button that clears the completed tasks
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
 
+// Declaring an empty array with the variable named tasks
 let tasks = [];
+
 
 function addTask() {
     const taskText = taskInput.value.trim();
